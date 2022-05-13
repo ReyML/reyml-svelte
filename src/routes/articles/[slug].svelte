b43fdb7a5bd96b0365d0125bf367a654962aaf0d
ReyML <rey117117@yahoo.com>
2022-05-13 21:34:39 +0400

    added style to article

--- src/routes/articles/[slug].svelte
+++ src/routes/articles/[slug].svelte
@@ -46,8 +46,33 @@
   <title>{post.title}</title>
 </svelte:head>
 
-<h1>{post.title}</h1>
-<p>{post.author.name}</p>
-<p>{post.date}</p>
-<p>{post.tags}</p>
-{@html post.content.html}
+<div class="mx-16 lg:mx-52 my-16 py-14">
+  <button class="my-4 rounded-md inter px-3 py-1 bg-[#1C1927]"
+    ><a href="/articles">Back</a></button
+  >
+
+  <h1 class="text-4xl font-semibold mb-7 text-gray-300">{post.title}</h1>
+  <a href="/" class="inline-flex items-center mb-6">
+    <span class="flex-grow flex flex-col pl-4">
+      <span class="font-medium text-gray-400">{post.author.name}</span>
+    </span></a
+  >
+  <div class="mb-6 flex justify-between">
+    <div>
+      {#if post.tags}
+        {#each post.tags as tag}
+          <span
+            class="py-1 px-2 mr-2 rounded bg-indigo-50 text-indigo-500 text-xs font-medium tracking-widest"
+            >{tag}</span
+          >
+        {/each}
+      {/if}
+    </div>
+    <p class="text-gray-400 text-xs tracking-widest mt-0.5">
+      {new Date(post.date).toDateString()}
+    </p>
+  </div>
+  <article class="text-gray-400 tracking-wider leading-7">
+    {@html post.content.html}
+  </article>
+</div>

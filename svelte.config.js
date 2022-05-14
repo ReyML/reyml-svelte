b555d78ff1dcce7d2185b1a0025cb886985274f5
ReyML <rey117117@yahoo.com>
2022-05-14 11:40:57 +0400

    removed blur effect

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,14 @@
 import vercel from "@sveltejs/adapter-vercel"
+import preprocess from "svelte-preprocess"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter: vercel(),
   },
+  preprocess: preprocess({
+    preserve: ["ld+json"],
+  }),
 }
 
 export default config

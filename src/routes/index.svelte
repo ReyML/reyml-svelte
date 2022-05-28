f171ccb6261387cd4851e0a9a7a8115e834ee26b
ReyML <rey117117@yahoo.com>
2022-05-28 15:01:05 +0400

    ui

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -92,105 +92,91 @@
   }}
 />
 
-
-    <div id='parallax' class="flex w-full justify-center items-center">
-      <div class="max-w-sm md:max-w-lg w-full mx-10 translate-y-48 md:translate-y-0">
-        <div class=" flex justify-center md:justify-end ">
-          <img
-            alt="profile pic"
-            class="w-20 h-20 object-cover rounded-full border-2 border-indigo-500"
-            src="/me1.jpg"
-          />
-        </div>
-        <div class="tracking-wider ">
-          <h2
-            class:is-active={active}
-            class="text-gray-300 text-3xl font-semibold inter active"
-          >
-            Web Developer
-          </h2>
-          <h2 class="font-sans">Why hire me</h2>
-          <ul class="mt-2 text-gray-400 font-sans text-md">
-            <li>
-              <span class="text-red-200 font-bold">Communicative</span> - I come
-              from a customer service background so I have a deep appreciation
-              for clear communication. I keep my clients updated frequently and
-              I can translate technical concepts into words that anyone can
-              understand.
-            </li>
-            <li>
-              <span class="text-red-200 font-bold">Detail orientated</span> - I am
-              thorough and caring to ensure that the final product matches your specifications.
-            </li>
-            <li>
-              <span class="text-red-200 font-bold">Time</span> - I understand the
-              importance of projects being delivered on time and I will treat deadlines
-              with maximum respect.
-            </li>
-          </ul>
-          <ul>
-            <br />
-            <p class="text-red-200">Core expertise</p>
-            <div class="flex flex-row flex-wrap gap-x-4 text-gray-400">
-              <li>Javascript</li>
-              <li>Tailwindcss</li>
-              <li>SvelteKit</li>
-              <li>React</li>
-              <li>11ty</li>
-              <li>GraphCMS</li>
-              <li>Firebase</li>
-            </div>
-          </ul>
-        </div>
-      </div>
-    </div>
-
-
-
 <canvas class="background" />
-
-<section class="bg-section h-screen items-center flex justify-center mx-8 ">
+<div id="parallax" class="flex w-full justify-center items-center">
   <div
-    class="container animatable"
-    id="projects"
-    style="animation-play-state: running;"
+    class="max-w-sm md:max-w-lg w-full mx-10 translate-y-48 md:translate-y-0"
   >
-    <div class="flex flex-col items-center w-full max-w-7xl m-auto">
+    <div class=" flex justify-center md:justify-end ">
+      <img
+        alt="profile pic"
+        class="w-20 h-20 object-cover rounded-full border-2 border-indigo-500"
+        src="/me1.jpg"
+      />
+    </div>
+    <div class="tracking-wider ">
       <h2
-        class="bg-clip-text text-transparent bg-gradient-to-r from-rose-400 to-pink-600 font-bold text-2xl sm:text-3xl md:text-4xl lg:text-5xl inter p-2 toProjects activee"
+        class:is-active={active}
+        class="text-gray-300 text-3xl font-semibold inter active"
+      >
+        Web Developer
+      </h2>
+      <h2 class="font-sans">Why hire me</h2>
+      <ul class="mt-2 text-gray-400 font-sans text-md">
+        <li>
+          <span class="text-red-200 font-bold">Communicative</span> - I come
+          from a customer service background so I have a deep appreciation for
+          clear communication. I keep my clients updated frequently and I can
+          translate technical concepts into words that anyone can understand.
+        </li>
+        <li>
+          <span class="text-red-200 font-bold">Detail orientated</span> - I am thorough
+          and caring to ensure that the final product matches your specifications.
+        </li>
+        <li>
+          <span class="text-red-200 font-bold">Time</span> - I understand the importance
+          of projects being delivered on time and I will treat deadlines with maximum
+          respect.
+        </li>
+      </ul>
+      <ul>
+        <br />
+        <p class="text-red-200">Core expertise</p>
+        <div class="flex flex-row flex-wrap gap-x-4 text-gray-400">
+          <li>Javascript</li>
+          <li>Tailwindcss</li>
+          <li>SvelteKit</li>
+          <li>React</li>
+          <li>11ty</li>
+          <li>GraphCMS</li>
+          <li>Firebase</li>
+        </div>
+      </ul>
+    </div>
+  </div>
+</div>
+
+<section class=" h-screen items-center flex justify-center">
+  <div class="my-16 mx-4 sm:mx-14 md:mx-24 lg:mx-72" id="projects" style="animation-play-state: running;">
+      <h2
+        class="text-center pb-3 bg-clip-text text-transparent bg-gradient-to-r from-rose-400 to-pink-600 font-bold text-2xl sm:text-3xl md:text-4xl lg:text-5xl inter toProjects activee"
       >
         Projects
       </h2>
-      <div class="flex flex-col items-center w-full mt-8">
-        <div class="lg:mx-56 my-2">
-          <a
-            href="https://urbanemoji.info"
-            target="_blank"
-            rel="noopener noreferrer"
-            class=" shadow-pink-500 flex h-52 md:h-72 justify-center items-center rounded-xl shadow-xl transition-all duration-300 hover:-translate-y-2"
-          >
-            <div
-              class="flex flex-col max-w-2xl justify-center items-start mt-4 px-5"
-            >
-              <p class="text-gray-400 flex font-mono">Javascript</p>
-              <h2
-                class="text-gray-400 text-xl sm:text-3xl font-bold font-heading text-center sm:text-left"
-              >
-                UrbanEmoji
-              </h2>
-              <p
-                class="text-gray-500 tracking-wider text-md sm:text-lg md:text-xl space mt-2"
-              >
-                The fastest way to get an Emoji combo or phrase. Just type and
-                search, from movies, meme, songs, anime, popular phrases and
-                much more, try it!.
-              </p>
-            </div>
-          </a>
+      <a
+        href="https://urbanemoji.info"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="md:p-10 p-4 sm:p-6 flex flex-col justify-center items-start shadow-pink-500 rounded-xl shadow-xl transition-all duration-300 hover:-translate-y-2"
+      >
+      <div class="md:mx-14">
+        <p class="text-gray-400 font-mono">Javascript</p>
+        <h2
+          class="text-gray-400 text-xl sm:text-3xl font-bold font-heading "
+        >
+          UrbanEmoji
+        </h2>
+        <p
+          class="text-gray-500 tracking-wider text-md sm:text-lg md:text-xl space mt-2 flex flex-wrap"
+        >
+          The fastest way to get an Emoji combo or phrase. Just type and search,
+          from movies, meme, songs, anime, popular phrases and much more, try
+          it!.
+        </p>
         </div>
-      </div>
+      </a>
     </div>
-  </div>
+
 </section>
 
 <style>
